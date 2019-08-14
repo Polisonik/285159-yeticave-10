31694ed3d38b8e663161087d5bd1f153bea4e0af
Alina <allred@inbox.ru>
2019-08-14 11:29:20 +0300

    Добавление функции форматирования цены

--- index.php
+++ index.php
@@ -42,6 +42,12 @@ $lots = [
     ]
 ];
 
+function format_price ($price) {
+  $price = ceil($price);
+  $price = number_format($price, 0, '.', ' ') . " <b class=\"rub\">р</b>";
+  return $price;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -118,7 +124,9 @@ $lots = [
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$lot['price']; ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=format_price($lot['price']); ?>
+                                <!--<b class="rub">р</b>-->
+                                </span>
                             </div>
                             <div class="lot__timer timer">
                             12:23
