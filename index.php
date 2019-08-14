91045d7658f430fe63c85152b076ac3d77e958ad
Alina <allred@inbox.ru>
2019-08-14 09:09:43 +0300

    Исправление короткого открывающегося тега <? на <?php

--- index.php
+++ index.php
@@ -112,21 +112,21 @@ $lots = [
                     <div class="lot__image">
                         <img src="<?=$lot['image']; ?>" width="350" height="260" alt="<?=$lot['name']; ?>">
                     </div>
-                <div class="lot__info">
-                    <span class="lot__category"><?=$lot['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['name']; ?></a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$lot['price']; ?><b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$lot['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['name']; ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$lot['price']; ?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
                             12:23
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
-            <? endforeach; ?>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
