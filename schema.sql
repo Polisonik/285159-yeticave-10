1bb19ae7af22c3b341ad6ce7d96f6400363da1d9
Alina <allred@inbox.ru>
2019-08-27 00:36:56 +0300

    Исправлены синтаксические ошибки запроса

--- schema.sql
+++ schema.sql
@@ -6,42 +6,41 @@ USE yeticave;
 
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR NOT NULL UNIQUE,
-    code VARCHAR
+    name VARCHAR(64) NOT NULL UNIQUE,
+    code VARCHAR(64)
 );
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    email  VARCHAR NOT NULL UNIQUE,
-    name CHAR(64) NOT NULL,
-    password CHAR(64) NOT NULL,
-    contacts VARCHAR NOT NULL
+    creation_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    email  VARCHAR(128) NOT NULL UNIQUE,
+    name VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    contacts VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE lots (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    date_add TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    name CHAR(64) NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    creation_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    end_time DATETIME NOT NULL DEFAULT  CURRENT_TIMESTAMP,
     description TEXT,
-    image_link CHAR(64),
-    price_start INT NOT NULL,
-    data_final TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    step_bid INT NOT NULL,
-    author INT NOT NULL,
-    winner INT NOT NULL,
-    category INT NOT NULL
+    image_link VARCHAR(128),
+    starting_price INT UNSIGNED NOT NULL,
+    step_bid INT UNSIGNED NOT NULL,
+    user_id INT NOT NULL,
+    winner_id INT,
+    category_id INT NOT NULL
 );
 
 CREATE TABLE bid (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    data_create TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    price INT NOT NULL,
+    creation_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    value INT NOT NULL,
     user_id INT NOT NULL,
     lot_id INT NOT NULL
 );
 
-CREATE UNIQUE INDEX email ON users(email);
 CREATE UNIQUE INDEX category_name ON categories(name);
 CREATE INDEX lot ON lots(name);
 
