974582ce73cd4ce69695cf84118d9b6917557461
Alina <allred@inbox.ru>
2019-08-29 22:10:38 +0300

    Добавление запросов с объединением таблиц

--- queries.sql
+++ queries.sql
@@ -44,9 +44,29 @@ SET creation_time = '2019-08-30 07:15:05', amount = 11500, user_id = 2, lot_id =
 SELECT * FROM categories;
 
 /*Вывод новых, открытых лотов. */
-SELECT l.name, creation_time, starting_price, image_link, c.name FROM lots l JOIN categories c ON l.category_id = c.id
-WHERE end_time > now() ORDER BY creation_time DESC;
+SELECT l.name, creation_time, starting_price, image_link, c.name
+FROM lots l
+JOIN categories c ON l.category_id = c.id
+WHERE end_time > now()
+ORDER BY creation_time DESC;
 
 /* показать лот по его id */
-SELECT l.name, creation_time, starting_price, image_link, c.name FROM lots l JOIN categories c ON l.category_id = c.id
+SELECT l.name, l.creation_time, starting_price, image_link, c.name, u.name, email
+FROM lots l
+JOIN categories c ON l.category_id = c.id
+join users u ON l.user_id = u.id
 WHERE l.id = 2;
+
+
+/* обновление названия лота по его идентификатору */
+UPDATE lots SET name = 'Крутая куртка для сноуборда DC Mutiny Charocal'
+Where id = 5;
+
+
+/* получение списка ставок для лота по его идентификатору с сортировкой по дате */
+SELECT b.creation_time, u.name, u.email, l.name, amount
+FROM bids b
+JOIN users u ON b.user_id = u.id
+JOIN lots l ON b.lot_id = l.id
+WHERE b.lot_id = 1
+ORDER BY b.creation_time DESC;
